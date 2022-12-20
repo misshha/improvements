c0c993f7193e56bf07b003b2c4b7f9c3d0664c08
ioonewa <112129053+ioonewa@users.noreply.github.com>
2022-12-20 09:12:49 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../src/styles/index.css';
+import App from './App';
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(
+  <App />
+)
+
